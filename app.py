283158f6330f2a6ad8befd789e97531bce68365d
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-07-08 23:10:33 -0500

    fix: fix

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from fastapi import FastAPI, File, UploadFile, Form
-from PIL import Image
-
 import os
-import io
-import uvicorn
 import pickle
+import random
 
+import uvicorn
+from fastapi import FastAPI, File, UploadFile, Form
+import json
 from fastapi.middleware.cors import CORSMiddleware
+
+from highd_query_manager import HighDQueryManager
 from rtree_query_manager import RTreeQueryManager
 from sequential_query_manager import SequentialQueryManager
-from highd_query_manager import HighDQueryManager
-
 
 app = FastAPI()
 
@@ -22,33 +21,37 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-with open("out.embeds", mode="rb") as collection_file:
-    collection = pickle.load(collection_file)
+managers = {
 
-sequential_query_manager = SequentialQueryManager(collection=collection)
-rtree_query_manager = RTreeQueryManager(collection=collection, m=3)
-high_d_query_manager = HighDQueryManager(collection=collection, num_bits=2000)
-temporary_query_path1 = "query1.jpg"
-temporary_query_path2 = "query2.jpg"
-temporary_query_path3 = "query3.jpg"
+}
+
+
+@app.on_event("startup")
+async def startup_event():
+    with open("out.embeds", mode="rb") as collection_file:
+        collection = pickle.load(collection_file)
+
+    managers["sequential"] = SequentialQueryManager(collection=collection)
+    managers["rtree"] = RTreeQueryManager(collection=collection, m=3)
+    managers["highd"] = HighDQueryManager(collection=collection, num_bits=2000)
 
 
 @app.post('/sequential_range')
-async def get_knn_sequential(file: UploadFile = File(...), r: str = Form(...)) -> dict:
+async def get_range_sequential(file: UploadFile = File(...), r: str = Form(...)) -> dict:
     r = float(r)
     data = await file.read()
 
-    with open(temporary_query_path1, 'wb') as f:
+    with open("sequential_query.jpg", 'wb') as f:
         f.write(data)
 
-    result = sequential_query_manager.range_query(q=temporary_query_path1, r=r)
-    os.remove(temporary_query_path1)
+    result = managers["sequential"].range_query(q="sequential_query.jpg", r=r)
+    os.remove("sequential_query.jpg")
 
     print(result)
     return {
-        'result': result,
+        'result': json.dumps(result),
         'response': 200
-        }
+    }
 
 
 @app.post('/sequential')
@@ -56,17 +59,17 @@ async def get_knn_sequential(file: UploadFile = File(...), k: str = Form(...)) -
     k = int(k)
     data = await file.read()
 
-    with open(temporary_query_path1, 'wb') as f:
+    with open("sequential_query.jpg", 'wb') as f:
         f.write(data)
 
-    result = sequential_query_manager.knn_query(q=temporary_query_path1, k=k)
-    os.remove(temporary_query_path1)
+    result = managers["sequential"].knn_query(q="sequential_query.jpg", k=k)
+    os.remove("sequential_query.jpg")
 
     print(result)
     return {
-        'result': result,
+        'result': json.dumps(result),
         'response': 200
-        }
+    }
 
 
 @app.post('/rtree')
@@ -74,16 +77,16 @@ async def get_knn_tree(file: UploadFile = File(...), k: str = Form(...)) -> dict
     k = int(k)
     data = await file.read()
 
-    with open(temporary_query_path1, 'wb') as f:
+    with open("rtree_query.jpg", 'wb') as f:
         f.write(data)
 
-    result = rtree_query_manager.knn_query(q=temporary_query_path2, k=k)
-    os.remove(temporary_query_path2)
+    result = managers["rtree"].knn_query(q="rtree_query.jpg", k=k)
+    os.remove("rtree_query.jpg")
 
     return {
-        'result': result,
+        'result': json.dumps(result),
         'response': 200
-        }
+    }
 
 
 @app.post('/highd')
@@ -91,17 +94,17 @@ async def get_knn_highd(file: UploadFile = File(...), k: str = Form(...)) -> dic
     k = int(k)
     data = await file.read()
 
-    with open(temporary_query_path1, 'wb') as f:
+    with open("highd_query.jpg", 'wb') as f:
         f.write(data)
 
-    result = high_d_query_manager.knn_query(q=temporary_query_path3, k=k)
-    os.remove(temporary_query_path3)
+    result = managers["highd"].knn_query(q="highd_query.jpg", k=k)
+    os.remove("highd_query.jpg")
 
     return {
-            'result': result,
-            'response': 200
-        }
+        'result': result,
+        'response': 200
+    }
 
 
 if __name__ == '__main__':
-    uvicorn.run('app:app', host='0.0.0.0', reload=True, port=8000)
+    uvicorn.run('app:app', host='0.0.0.0', port=8000)
