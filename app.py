8c91b732379c383141829d235d1efce3cc93a492
juandiego <juan.castro.p@utec.edu.pe>
2023-07-08 21:06:27 -0500

    feat: backend :D

---
+++ app.py
@@ -0,0 +1,89 @@
+from fastapi import FastAPI, File, UploadFile, Form
+from PIL import Image
+
+import os
+import io
+import uvicorn
+import pickle
+
+from fastapi.middleware.cors import CORSMiddleware
+from rtree_query_manager import RTreeQueryManager
+from sequential_query_manager import SequentialQueryManager
+from highd_query_manager import HighDQueryManager
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+with open("out.embeds", mode="rb") as collection_file:
+    collection = pickle.load(collection_file)
+
+sequential_query_manager = SequentialQueryManager(collection=collection)
+rtree_query_manager = RTreeQueryManager(collection=collection, m=3)
+high_d_query_manager = HighDQueryManager(collection=collection, num_bits=2000)
+temporary_query_path1 = "query1.jpg"
+temporary_query_path2 = "query2.jpg"
+temporary_query_path3 = "query3.jpg"
+
+
+@app.post('/sequential')
+async def get_knn_sequential(file: UploadFile = File(...), k: str = Form(...)) -> dict:
+    k = int(k)
+    data = await file.read()
+
+    with open(temporary_query_path1, 'wb') as f:
+        f.write(data)
+
+    result = sequential_query_manager.knn_query(q=temporary_query_path1, k=k)
+    os.remove(temporary_query_path1)
+
+    print(result)
+    return {
+        'result': result,
+        'response': 200
+        }
+
+
+@app.post('/rtree')
+async def get_knn_tree(file: UploadFile = File(...), k: str = Form(...)) -> dict:
+    k = int(k)
+    data = await file.read()
+
+    with open(temporary_query_path1, 'wb') as f:
+        f.write(data)
+
+    result = rtree_query_manager.knn_query(q=temporary_query_path2, k=k)
+    os.remove(temporary_query_path2)
+
+    return {
+        'result': result,
+        'response': 200
+        }
+
+
+@app.post('/highd')
+async def get_knn_highd(file: UploadFile = File(...), k: str = Form(...)) -> dict:
+    k = int(k)
+    data = await file.read()
+
+    with open(temporary_query_path1, 'wb') as f:
+        f.write(data)
+
+    result = high_d_query_manager.knn_query(q=temporary_query_path3, k=k)
+    os.remove(temporary_query_path3)
+
+    return {
+            'result': result,
+            'response': 200
+        }
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', reload=True)
--- test.py
+++ test.py
@@ -10,7 +10,8 @@ def print_result(result):
     for i in result:
         for j in i:
             print(j)
-    print("\n\n")
+        print("")
+    print("\n")
 
 
 def main() -> None:
@@ -21,7 +22,7 @@ def main() -> None:
     rtree_query_manager = RTreeQueryManager(collection=collection, m=3)
     high_d_query_manager = HighDQueryManager(collection=collection, num_bits=2000)
 
-    query = os.path.join(os.getcwd(), "lfw/Adam_Sandler/Adam_Sandler_0002.jpg")
+    query = os.path.join(os.getcwd(), "lfw/Abdullatif_Sener/Abdullatif_Sener_0002.jpg")
 
     print_result(sequential_query_manager.knn_query(q=query, k=4))
     print_result(rtree_query_manager.knn_query(q=query, k=4))
