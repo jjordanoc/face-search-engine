36540b331bf0e52808157ad10b30410a2625384a
Juan Diego <68095284+DarKNeSsJuaN25@users.noreply.github.com>
2023-07-08 20:45:09 -0500

    Add files via upload

--- front/src/App.js
+++ front/src/App.js
@@ -1,47 +1,49 @@
 import './App.css';
 import { useRef, useState } from 'react';
+
 function App() {
+  const [image, setimage] = useState(null);
+  const fileInput = useRef(null);
+  const inputRef = useRef(null);
 
-  const [image, setimage] = useState(null)
-  const fileInput = useRef(null)
+  const handleImage = async () => {
+    const file = fileInput.current.files[0];
+    const k = inputRef.current.value || '1';
 
-  const handleImage = async() => {
-    const file = fileInput.current.files[0] 
+    const formData = new FormData();
+    formData.append('image', file);
+    formData.append('k',k);
 
-    const formData = new FormData()
-    formData.append('image', file)
+    try {
+      const response = await fetch('http://localhost:8000/procesar_imagen', {
+        method: 'POST',
+        body: formData,
+      });
 
-    try{
-      const response = await fetch('http://localhost:8000/procesar_imagen',{
-        method :'POST',
-        body : formData, 
-        headers : {
-        }
-      })
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data.message); // Imprime el mensaje recibido en la consola
-    } else {
-      console.log("Error");
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData.message); // Imprime el mensaje recibido en la consola
+      } else {
+        console.log('Error');
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
-    setimage(URL.createObjectURL(file))
-  }
 
-
+    setimage(URL.createObjectURL(file));
+  };
 
   return (
     <div className="App">
       <h3>Proyecto 3</h3>
-      <input type='file' ref={fileInput} /> 
+      <input type="file" ref={fileInput} />
+      <input type="text" placeholder="K" ref={inputRef} />
+      <button onClick={handleImage}>Enviar</button>
       {image && (
         <div>
-          <img src={image} alt='imagen-seleccionada'/>
-          </div>
-      )} 
-      <button onClick={handleImage}>Enviar</button>
+          <img src={image} alt="imagen-seleccionada" />
+        </div>
+      )}
     </div>
   );
 }
